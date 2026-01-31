894cb63b41e944a6452ae708d114d068e1914000
Never-Error404 <Shootingalltime@gmail.com>
2026-01-31 21:19:01 +0530

    new

--- src/Patterns/Pattern_33.java
+++ src/Patterns/Pattern_33.java
@@ -2,15 +2,16 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class Pattern_32 {
+public class Pattern_33 {
+    /*
+       E D C B A
+       D C B A
+       C B A
+       B A
+       A
+
+     */
     static void main(String[] args) {
-        /*
-       a
-       B c
-       D e F
-       g H i J
-       k L m N o
-         */
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the size of pattern: ");
@@ -19,12 +20,10 @@ public class Pattern_32 {
 
     }
     static void pattern(int n){
-        int num = 0;
         for(int i =1; i<=n; i++){
-            for(int j = 1; j<= i; j++){
-                char letter = num % 2 == 1? (char) ('A' + (num%26)) : (char)('a' + (num% 26));
+            for(int j = 1; j<= n-i+1; j++){
+                char letter = (char) ('A' + n-j);
                 System.out.print(letter + " ");
-                num++;
             }
             System.out.println();
         }
