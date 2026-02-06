e446edbd4d9c1dbbe0902362c0b33e0f9925df7b
Never-Error404 <Shootingalltime@gmail.com>
2026-02-06 22:24:05 +0530

    new

---
+++ Assignments/src/Searching/Medium/Find_Duplicate_Number.java
@@ -0,0 +1,24 @@
+package Searching.Medium;
+
+public class Find_Duplicate_Number {
+    static void main(String[] args) {
+        int[] nums = {1,3,4,2,2};
+        System.out.println(findDuplicate(nums));
+    }
+    static public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
