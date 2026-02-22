bd7b7a86bd04b02bae9a09f3f956f6b9463ca002
Never-Error404 <Shootingalltime@gmail.com>
2026-02-22 18:39:18 +0530

    question

---
+++ Assignments/src/Sorting/Easy/Intersection_of_Two_Arrays.java
@@ -0,0 +1,69 @@
+package Sorting.Easy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Intersection_of_Two_Arrays {
+//    LeetCode: 349. Intersection of Two Arrays
+
+    /*
+    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+
+            Example 1:
+            Input: nums1 = [1,2,2,1], nums2 = [2,2]
+            Output: [2]
+
+            Example 2:
+            Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+            Output: [9,4]
+            Explanation: [4,9] is also accepted.
+
+            Constraints:
+            1 <= nums1.length, nums2.length <= 1000
+            0 <= nums1[i], nums2[i] <= 1000
+     */
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of array 1: ");
+        int size1 = input.nextInt();
+        int[] nums1 = new int[size1];
+        System.out.println("Enter the elements of the array 1: ");
+        for (int i = 0; i<size1; i++){
+            nums1[i] = input.nextInt();
+        }
+        System.out.println("Enter the size of array 2: ");
+        int size2 = input.nextInt();
+        int[] nums2 = new int[size2];
+        System.out.println("Enter the elements of the array 2: ");
+        for (int i = 0; i<size2; i++){
+            nums2[i] = input.nextInt();
+        }
+        System.out.println(Arrays.toString(intersection(nums1,nums2)));
+    }
+
+    static public int[] intersection(int[] nums1, int[] nums2) {
+        int[] freq = new int[1001];
+
+        for(int n : nums1){
+            freq[n] = 1;
+        }
+
+        int[] temp = new int[Math.min(nums1.length, nums2.length)];
+        int k = 0;
+        for(int n: nums2){
+            if(freq[n] == 1){
+                freq[n] = 0;
+                temp[k++] = n;
+            }
+
+        }
+
+        int[] ans = new int[k];
+        for(int i = 0; i<k; i++){
+            ans[i] = temp[i];
+        }
+        return ans;
+
+    }
+}
---
+++ Assignments/src/Sorting/Easy/Intersection_of_Two_Arrays_II.java
@@ -0,0 +1,73 @@
+package Sorting.Easy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Intersection_of_Two_Arrays_II {
+
+//    LeetCode: 350. Intersection of Two Arrays II
+
+    /*
+
+    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+
+
+            Example 1:
+            Input: nums1 = [1,2,2,1], nums2 = [2,2]
+            Output: [2,2]
+
+            Example 2:
+            Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+            Output: [4,9]
+            Explanation: [9,4] is also accepted.
+
+
+            Constraints:
+            1 <= nums1.length, nums2.length <= 1000
+            0 <= nums1[i], nums2[i] <= 1000
+     */
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of array 1: ");
+        int size1 = input.nextInt();
+        int[] nums1 = new int[size1];
+        System.out.println("Enter the elements of the array 1: ");
+        for (int i = 0; i<size1; i++){
+            nums1[i] = input.nextInt();
+        }
+        System.out.println("Enter the size of array 2: ");
+        int size2 = input.nextInt();
+        int[] nums2 = new int[size2];
+        System.out.println("Enter the elements of the array 2: ");
+        for (int i = 0; i<size2; i++){
+            nums2[i] = input.nextInt();
+        }
+        System.out.println(Arrays.toString(intersect(nums1,nums2)));
+    }
+    static public int[] intersect(int[] nums1, int[] nums2) {
+
+        int[] freq = new int[1001];
+
+        for (int n : nums1) {
+            freq[n]++;
+        }
+
+        int[] temp = new int[Math.min(nums1.length, nums2.length)];
+        int k = 0;
+
+        for (int n : nums2) {
+            if (freq[n] > 0) {
+                temp[k++] = n;
+                freq[n]--;
+            }
+        }
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = temp[i];
+        }
+
+        return result;
+    }
+}
